985c097b209db33023f6c5481f7514a4683a1388
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-03-25 20:29:48 +0300

    configured the bg image for mobile and as well as the screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1224px',
+      '2xl': '1824px'
+    },
+    colors:{
+      'goldy': '#ff6600',
+      'white' : '#ffffff',
+      'black' : '#000000',
+    },
+    extend: {
+      backgroundImage: {
+        'mobileBg': "url('/img/background/smallBG.jpg')",
+      }
+    },
   },
   plugins: [],
-}
+};
