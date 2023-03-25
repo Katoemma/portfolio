ee9c99f1cbd0eb854f9206bac38142fba15393a9
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-03-25 20:28:40 +0300

    created the main javascript file and also animated the autotyping text

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// js to change navbar bg on scroll
+
+
+
+//js to automatically type the hero text
+function autoType(element, texts, delay = 100) {
+    let textIndex = 0;
+    let charIndex = 0;
+    function type() {
+      if (charIndex < texts[textIndex].length) {
+        element.innerHTML += texts[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, delay);
+      } else {
+        setTimeout(erase, delay);
+      }
+    }
+    function erase() {
+      if (charIndex > 0) {
+        element.innerHTML = texts[textIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, delay);
+      } else {
+        textIndex++;
+        if (textIndex >= texts.length) {
+          textIndex = 0;
+        }
+        setTimeout(type, delay);
+      }
+    }
+    setTimeout(type, delay);
+  }
+  
+  const typedText = document.querySelector("#Ctext");
+  autoType(typedText, ["developer.", "web designer.", "student."]);
+  
